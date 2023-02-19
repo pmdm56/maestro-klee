31b34db62155ab65735a6fff87ef54d9cd151b10
davidbaptista <david.baptista00@gmail.com>
2023-02-19 22:43:42 +0000

    Added Visitor; Beginning BDDtraversal

---
+++ tools/synergio/bdd/visitor.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "call-paths-to-bdd.h"
+
+namespace Synergio {
+	class Visitor: public BDD::BDDVisitor {
+		protected:
+			virtual Action visitBranch(const BDD::Branch *node) override;
+			virtual Action visitCall(const BDD::Call *node) override;
+			virtual Action visitReturnInit(const BDD::ReturnInit *node) override;
+			virtual Action visitReturnProcess(const BDD::ReturnProcess *node) override;
+			virtual Action visitReturnRaw(const BDD::ReturnRaw *node) override;
+		public:
+			Visitor();
+	};
+}
--- tools/synergio/models/network.hpp
+++ tools/synergio/models/network.hpp
@@ -32,6 +32,11 @@ namespace Synergio {
 		Network(Devices &&devices, NFs &&nfs, Links &&links, BDDs &&bdds);
 
 		NodeType get_node_type(const std::string &node_str) const;
+
+		void consolidate_device_to_device(const std::string &device_from, const unsigned port_from, const std::string &device_to, const unsigned port_to);
+		void consolidate_nf_to_device(const std::string &nf_from, const unsigned port_from, const std::string &device_to, const unsigned port_to);
+		void consolidate_device_to_nf(const std::string &device_from, const unsigned port_from, const std::string &nf_to, const unsigned port_to);
+		void consolidate_nf_to_nf(const std::string &nf_from, const unsigned port_from, const std::string &nf_to, const unsigned port_to);
 	public:
 		~Network();
 
