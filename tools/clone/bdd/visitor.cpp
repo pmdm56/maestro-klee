5ed2fef784f0dc484e654ea1bf0e1bb15290fb37
davidbaptista <david.baptista00@gmail.com>
2023-02-27 21:21:10 +0000

    remove prints

--- tools/clone/bdd/visitor.cpp
+++ tools/clone/bdd/visitor.cpp
@@ -2,6 +2,7 @@
 #include "../pch.hpp"
 #include "../util/logger.hpp"
 
+#include "klee/Constraints.h"
 #include "call-paths-to-bdd.h"
 
 #include "bdd-builder.hpp"
@@ -18,24 +19,37 @@ namespace Clone {
 
 	void Visitor::visitInitRoot(const BDD::Node *root) {
 		debug("Visiting init root");
+
+		// TODO: add constraints (or not)
 		root->visit(*this);
 	}
 
 	void Visitor::visitProcessRoot(const BDD::Node *root) {
 		debug("Visiting process root");
+
+		// TODO: add constraints
 		root->visit(*this);
 	}
 
 	BDD::BDDVisitor::Action Visitor::visitBranch(const BDD::Branch *node) {
 		debug("Visiting branch");
+		//node->clone();
 
-		auto &constraints = node->get_constraints();
+		auto &constraints_list = node->get_constraints();
+
+		for(auto &constraints: constraints_list) {
+			for(auto &constraint: constraints) {
+
+			}
+		}
 
 		return Action::VISIT_CHILDREN;
 	}
 
 	BDD::BDDVisitor::Action Visitor::visitCall(const BDD::Call *node) {
 		debug("Visiting call");
+
+		info(node->get_call().function_name); // TODO: check if its a function that reads input (?)
 
 
 		return Action::VISIT_CHILDREN;
